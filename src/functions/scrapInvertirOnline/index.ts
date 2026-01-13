beefbc249e9b3b2fa154496856ffa8535844247f
Leandro Martin Soler <martosoler@gmail.com>
2026-01-12 22:47:35 -0300

    lint fixes

--- src/functions/scrapInvertirOnline/index.ts
+++ src/functions/scrapInvertirOnline/index.ts
@@ -1,9 +1,8 @@
-import logger from "../../shared/logger";
+import logger from '../../shared/logger';
 
-export async function scrapInvertirOnline(
-): Promise<null> {
-    logger.info('InvertirOnline scraping function called');
+export async function scrapInvertirOnline(): Promise<null> {
+  logger.info('InvertirOnline scraping function called');
 
-    logger.info('InvertirOnline scraping function completed');
-    return null;
+  logger.info('InvertirOnline scraping function completed');
+  return null;
 }
