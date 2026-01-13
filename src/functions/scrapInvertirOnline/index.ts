fb246dbd051fa3a762907c17c2103adafbe4781e
Leandro Martin Soler <martosoler@gmail.com>
2026-01-12 22:44:47 -0300

    Adding InvertirOnline scrapper cloud function boilerplate

---
+++ src/functions/scrapInvertirOnline/index.ts
@@ -0,0 +1,9 @@
+import logger from "../../shared/logger";
+
+export async function scrapInvertirOnline(
+): Promise<null> {
+    logger.info('InvertirOnline scraping function called');
+
+    logger.info('InvertirOnline scraping function completed');
+    return null;
+}
